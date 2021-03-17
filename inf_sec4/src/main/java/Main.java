2af5b58cbae0573deaab536d5e125f26501d7a3a
StasMalikov <stas.malikov75@gmail.com>
2021-03-17 08:56:08 +0300

    init lab4

---
+++ inf_sec4/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import at.favre.lib.bytes.Bytes;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.print("Введите длинну последовательности чисел: ");
+//        Scanner in = new Scanner(System.in);
+//        int count = in.nextInt();
+//        MyRandom random = new MyRandom();
+//
+//        MyParams params1 = new MyParams(8, 9, 1, 12);
+//        MyParams params2 = new MyParams(10, 7, 7, 7);
+//
+//        List<Long> result1 = random.generate(count);
+//        List<Long> result2 = random.generate(count, params1);
+//        List<Long> result3 = random.generate(count, params2);
+//
+//        System.out.println("result1 " + result1.toString());
+//        test1(result1);
+//        System.out.println("result3 " + result3.toString());
+//        test1(result3);
+//        System.out.println("result2 " + result2.toString());
+//        test1(result2);
+    }
+}
